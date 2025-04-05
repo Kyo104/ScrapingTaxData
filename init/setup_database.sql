d888e1ae9c0ffe3cfd0cc8f14dd203e7f9d2b100
Kyo104 <116028457+Kyo104@users.noreply.github.com>
2025-04-05 15:32:07 +0700

    add sample and remove comment

--- init/setup_database.sql
+++ init/setup_database.sql
@@ -11,12 +11,9 @@ CREATE TABLE company_information (
     bhxh_password VARCHAR(100)       -- Mật khẩu BHXH
 );
 
--- Insert diverse sample data with different cases
+
 INSERT INTO company_information (company_id, company_name, thue_username, thue_password, hoadon_username, hoadon_password, bhxh_username, bhxh_password) 
 VALUES 
-    -- Case 1: Company using all three services (Thuế, Hóa đơn, BHXH)
-    ('1001', 'Công ty Alpha', '0101652097-ql', 'At2025$$', '0101652097', 'At2026@@@', '0101850613', '@ATDT2024'),
-    ('1002', 'Công ty Alpha2', '0101652097-ql', 'At2025$$', '0101652097', 'At2026@@@', '0101850613', '@ATDT2024'),
-    ('1003', 'Công ty Alpha3', '0101652097-ql', 'At2025$$', '0101652097', 'At2026@@@', '0101850613', '@ATDT2024'),
-    ('1004', 'Công ty Alpha4', '0101652097-ql', 'At2025$$', '0101652097', 'At2026@@@', '0101850613', '@ATDT2024');
+    ('1001', 'Công ty A1', '0101652097-ql', 'At2025$$', '0100995373', 'sa4%5AG@', '0101850613', '@ATDT2024'),
+    ('1002', 'Công_ty_Alpha4', '0101652097-ql', 'At2025$$', '0101652097', 'At2026@@@', '0101850613', '@ATDT2024');
     
