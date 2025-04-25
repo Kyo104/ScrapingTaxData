a843ea91b5854d7444a5b7ddf94581dbaba00899
Kyo104 <116028457+Kyo104@users.noreply.github.com>
2025-04-25 16:23:40 +0700

    Update setup_database.sql

--- init/setup_database.sql
+++ init/setup_database.sql
@@ -11,9 +11,13 @@ CREATE TABLE company_information (
     bhxh_password VARCHAR(100)       -- Mật khẩu BHXH
 );
 
-
 INSERT INTO company_information (company_id, company_name, thue_username, thue_password, hoadon_username, hoadon_password, bhxh_username, bhxh_password) 
 VALUES 
-    ('1001', 'Công ty A1', '0101652097-ql', 'At2025$$', '0100995373', 'sa4%5AG@', '0101850613', '@ATDT2024'),
-    ('1002', 'Công ty B2', NULL, NULL, NULL, NULL, '0101850613', '@ATDT2024');
-    
+    ('1001', 'AT_CP_1', '0101357599-ql', '@At2025', '0101357599', '$q$$1C@#', '0101357599', '@ATcp2024'),
+    ('1002', 'AT_CP_2', NULL, NULL, '0101357599', '$q$$1C@#', '0101357599', '@ATcp2024'),
+    ('1003', 'AT_CP_3', '0101357599-ql', '@At2025', NULL, NULL, '0101357599', '@ATcp2024'),
+    ('1004', 'AT_CP_4', '0101357599-ql', '@At2025', '0101357599', '$q$$1C@#', NULL, NULL),
+    ('1005', 'AT_CP_5', NULL, NULL, NULL, NULL, '0101357599', '@ATcp2024'),
+    ('1006', 'AT_CP_6', '0101357599-ql', '@At2025', NULL, NULL, NULL, NULL),
+    ('1007', 'AT_CP_7', NULL, NULL, '0101357599', '$q$$1C@#', NULL, NULL),
+    ('1008', 'AT_CP_8', NULL, NULL, NULL, NULL, NULL, NULL);
