9146e4c973dd4691a6c062ff5067fb2b7989155c
LanTran_vlu <trannguyenthanhlan@vanlanguni.vn>
2025-04-27 09:04:46 +0700

    LT: Update jenkins config and app args for separate module selection.

--- app/crawler_app.py
+++ app/crawler_app.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 import os
 
@@ -8,29 +9,36 @@ from src.crawler_thuedientu import crawler_thuedientu
 from src.crawler_hoadondientu import crawler_hoaddondientu
 
 if __name__ == '__main__':
-    try:
-        crawler_bhxh = crawler_baohiemxahoi()
-        crawler_bhxh.main_logic()
-    except Exception as e:
-        print("\n" + "="*50)
-        print("[❌] Cảnh báo ở crawler_baohiemxahoi")
-        print(f"Chi tiết cảnh báo: {e}")
-        print("="*50 + "\n")
+    parser = argparse.ArgumentParser(description="Module selection")
+    parser.add_argument("--module", default="ALL", required=False, help="Chọn module cần chạy")
+    module = parser.parse_args()
+    
+    if module == "BHXH" or module == "ALL":
+        try:
+            crawler_bhxh = crawler_baohiemxahoi()
+            crawler_bhxh.main_logic()
+        except Exception as e:
+            print("\n" + "="*50)
+            print("[❌] Cảnh báo ở crawler_baohiemxahoi")
+            print(f"Chi tiết cảnh báo: {e}")
+            print("="*50 + "\n")
 
-    try:
-        crawler_tdt = crawler_thuedientu()
-        crawler_tdt.main_logic()
-    except Exception as e:
-        print("\n" + "="*50)
-        print("[❌] Cảnh báo ở crawler_thuedientu")
-        print(f"Chi tiết cảnh báo: {e}")
-        print("="*50 + "\n")
-
-    try:
-        crawler_hddt = crawler_hoaddondientu()
-        crawler_hddt.main_logic()
-    except Exception as e:
-        print("\n" + "="*50)
-        print("[❌] Cảnh báo ở crawler_hoaddondientu")
-        print(f"Chi tiết cảnh báo: {e}")
-        print("="*50 + "\n")
+    if module == "TDT" or module == "ALL":
+        try:
+            crawler_tdt = crawler_thuedientu()
+            crawler_tdt.main_logic()
+        except Exception as e:
+            print("\n" + "="*50)
+            print("[❌] Cảnh báo ở crawler_thuedientu")
+            print(f"Chi tiết cảnh báo: {e}")
+            print("="*50 + "\n")
+    
+    if module == "HDDT" or module == "ALL":
+        try:
+            crawler_hddt = crawler_hoaddondientu()
+            crawler_hddt.main_logic()
+        except Exception as e:
+            print("\n" + "="*50)
+            print("[❌] Cảnh báo ở crawler_hoaddondientu")
+            print(f"Chi tiết cảnh báo: {e}")
+            print("="*50 + "\n")
